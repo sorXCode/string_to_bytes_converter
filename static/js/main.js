4377aac1c7a43469f886ee5193b08318d77ea4ac
sorxcode <sorxcode@gmail.com>
2022-01-19 15:35:33 +0100

    [refactored]

--- static/js/main.js
+++ static/js/main.js
@@ -1,26 +1,34 @@
 $("#str_to_hex").change(function() {
     if (!this.checked){
-        $("#encoding").parent().hide()
-        $("#add_prefix").parent().hide()
-        $("#text").prop("required", false)
-        $("#result").prop("required", true)
+        bytesInputMode()
     }
     else {
-        $("#encoding").parent().show()
-        $("#add_prefix").parent().show()
-        $("#text").prop("required", true)
-        $("#result").prop("required", false)
+        textInputMode()
     }
 }
 )
 
 $("#close").click( function(){
-    console.log("closed");
     $(".alert-message").hide();
 }
 )
 
-$(function() {
+function textInputMode(){
+    $("#encoding").parent().show()
+    $("#add_prefix").parent().show()
     $("#text").prop("required", true)
     $("#result").prop("required", false)
+    $('button[type="submit"]').text("Convert Text to Bytes")
+}
+
+function bytesInputMode(){
+    $("#encoding").parent().hide()
+    $("#add_prefix").parent().hide()
+    $("#text").prop("required", false)
+    $("#result").prop("required", true)
+    $('button[type="submit"]').text("Convert Bytes to Text")
+}
+
+$(function() {
+    textInputMode()
 })
